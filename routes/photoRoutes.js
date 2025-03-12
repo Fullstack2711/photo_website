59ea014f56fe58df9171d19ab47cd66387971c99
Ibrohimov-Zafar <ibragimovzafar001@gmail.com>
2025-03-12 10:02:21 +0500

    Yangilangan kodlar

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { getPhotos, addPhoto, deletePhoto, myPhotos } = require("../controllers/photoController");
 const { checkToken } = require("../middlewere/check");
+const uploadMiddleware = require("../middlewere/uploadsMiddlewere");
 const photoRouter = express.Router();
 
-photoRouter.post('/', checkToken, addPhoto);
-photoRouter.get('/',   getPhotos);
+photoRouter.post('/', checkToken,uploadMiddleware ,addPhoto);
+photoRouter.get('/', getPhotos);
 photoRouter.get('/:userId', checkToken, myPhotos);
 photoRouter.delete('/:id', checkToken, deletePhoto);
 
-module.exports = photoRouter;
+
+module.exports = photoRouter   
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 // Importing routes
 const userRouter = require("./routes/userRoutes");
 const photoRouter = require("./routes/photoRoutes");
-const likeRouter = require("./routes/likeRouter");
+const likeRouter = require("./routes/likeRoutes"); // Fayl nomiga e’tibor bering
 
 // Middleware
 app.use(express.json());
@@ -12,8 +13,11 @@ app.use(cors());
 
 // Calling routes
 app.use('/', userRouter);
-app.use('/photos', photoRouter)
-app.use('/', likeRouter)
+app.use('/photos', photoRouter);
+app.use('/likes', likeRouter); // '/likes' bilan chaqirish kerak
+
+// Static files
+app.use("/uploads", express.static("uploads"));
 
 const port = 4000;
 app.listen(port, () => {
